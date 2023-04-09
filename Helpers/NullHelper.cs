da2571829c44f4ad8ad8e19896a152dcac50758a
LukeHjo <lukehjo@gmail.com>
2023-04-10 01:38:24 +0200

    New update.

---
+++ Helpers/NullHelper.cs
@@ -0,0 +1,11 @@
+﻿internal static class NullHelper
+{
+
+    public static void Patch(string[] args)
+    {
+        if (args is null)
+        {
+            throw new ArgumentNullException(nameof(args));
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,102 +1,122 @@
-﻿using System;
-using System.Diagnostics;
-using System.Management;
+﻿using System.Diagnostics;
+using System.Threading;
 
 namespace ProcLogger;
 
-/// <summary>
-/// Process Logger
-/// </summary>
-/// <returns></returns>
 internal class Program
 {
-    private static async Task Main(string[] args)
+    private Dictionary<string, int> processDict = new();
+
+    private Process[] processes = new Process[Process.GetProcesses().Length];
+
+    public Process[] Processes
     {
-        CIN(args);
-
-        await LogAllProcesses(2);
-
-        
+        get => processes;
+        set => processes = value;
+    }
+    public Dictionary<string, int> ProcessDict
+    {
+        get => processDict;
+        set => processDict = value;
     }
 
-    public static void CIN(string[] args)
+    private static async Task Main()
     {
-        if (args is null)
-        {
-            throw new ArgumentNullException(nameof(args));
-        }
-    }
-
-    private static async Task LogAllProcesses(int _case)
-    {
-        var processDict = new Dictionary<string, int>();
+        Dictionary<string, int> processDict = new();
         var processes = new Process[Process.GetProcesses().Length];
-        Process.GetProcesses().CopyTo(processes, 0);
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.Title = "Process Logger";
+        await LogAllProcesses(processes, processDict);
+    }
 
 
-        switch (_case)
+
+    private static async Task LogAllProcesses(Process[] Processes, Dictionary<string, int> ProcessDict)
+    {
+        
+        Process.GetProcesses().CopyTo(Processes, 0);
+        await PreviousProcesses(ProcessDict, Processes);
+        await NewProcesses(ProcessDict, Processes);
+
+        static async Task NewProcesses(Dictionary<string, int> processDict, Process[] processes)
         {
-            case 0:
-                await OrgProc(processDict, processes);
-                break;
-            case 1:
-                processes = await NotOrgProc(processDict);
-                break;
-            case 2:
-                await OrgProc(processDict, processes);
-                processes = await NotOrgProc(processDict);
-                break;
+            try
+            {
+                while (true)
+                {
+                    var updatedProcessDict = new Dictionary<string, int>();
+                    for (var i = 0; i < processes.Length; i++)
+                    {
+                        var process = processes[i];
+                        if (!updatedProcessDict.ContainsKey(process.ProcessName))
+                        {
+                            updatedProcessDict.Add(process.ProcessName, process.Id);
+                        }
+                    }
 
+                    // Display the newly created Processes
+                    foreach (var kvp in updatedProcessDict)
+                    {
+                        var v = Process.GetProcessById(kvp.Value);
+                        var handleCount = (ushort)(v.HandleCount);
+                        if (!processDict.ContainsKey(kvp.Key))
+                        {
+                            processDict.Add(kvp.Key, kvp.Value);
+                            if (handleCount > 0)
+                            {
+                                var ramUsage = (uint)(v.WorkingSet64 / 1000000);
+                                var threads = (ushort)(v.Threads.Count);
+                                if (kvp.Value > 0)
+                                {
+                                    Console.WriteLine(DateTime.Now + " | {0} | Process ID: {1} | Total RAM Usage: {2} mb | Total Threads: {3} | Total Handles: {4}", kvp.Key, kvp.Value, ramUsage, threads, handleCount);
+                                }
+                            }
+                        }
+                        await Task.Delay(10);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
 
-        
 
-        static async Task OrgProc(Dictionary<string, int> processDict, Process[] processes)
+        static async Task PreviousProcesses(Dictionary<string, int> processDict, Process[] processes)
         {
+            Console.WriteLine("\n");
             foreach (var process in processes)
             {
-                if (!processDict.ContainsKey(process.ProcessName))
+                if(process?.Id > 0)
                 {
-                    processDict.Add(process.ProcessName, process.Id);
+                    if (!processDict.ContainsKey(process.ProcessName))
+                    {
+                        processDict.Add(process.ProcessName, process.Id);
+                    }
                 }
             }
 
+
+            using var sw = new StreamWriter(@"output.txt");
             foreach (var kvp in processDict)
             {
-                await Task.Run(() => Console.WriteLine("B | " + DateTime.Now + " | {0} | Process ID: {1}", kvp.Key, kvp.Value));
+                if (kvp.Value > 0)
+                {
+                    var v = Process.GetProcessById(kvp.Value);
+                    var handleCount = (ushort)(v.HandleCount);
+                    if (handleCount > 0)
+                    {
+                        var ramUsage = (uint)(v.WorkingSet64 / 1000000);
+                        var threads = (ushort)(v.Threads.Count);
+                        await sw.WriteLineAsync($"{kvp.Key} | Process ID: {kvp.Value} | Total RAM Usage: {ramUsage} mb | Total Threads: {threads} | Total Handles: {handleCount}");
+                        await Task.Run(() => Console.WriteLine(DateTime.Now + " | {0} | Process ID: {1} | Total RAM Usage: {2} mb | Total Threads: {3} | Total Handles: {4}", kvp.Key, kvp.Value, ramUsage, threads, handleCount));
+                    }
+                }
                 Task.Delay(10).Wait();
-
-            }          
-        }
-
-        static async Task<Process[]> NotOrgProc(Dictionary<string, int> processDict)
-        {
-            Process[] processes;
-            while (true)
-            {
-                var updatedProcessDict = new Dictionary<string, int>();
-
-                processes = Process.GetProcesses();
-
-                foreach (var process in processes)
-                {
-                    if (!updatedProcessDict.ContainsKey(process.ProcessName))
-                    {
-                        updatedProcessDict.Add(process.ProcessName, process.Id);
-                    }
-                }
-
-                foreach (var kvp in updatedProcessDict)
-                {
-                    if (!updatedProcessDict.ContainsKey(kvp.Key))
-                    {
-                        Console.WriteLine("A | " + DateTime.Now + " | {0} | Process ID: {1}", kvp.Key, kvp.Value);
-                    }
-
-                }
-                await Task.Delay(1000);
-
             }
+
+
         }
     }
 }
